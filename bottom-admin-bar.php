88bdade1361976b116d5c0ecf0f5fbafa418df0a
Ettore Del Negro <ettore@themecraft.studio>
2017-05-12 13:25:11 +0200

    Plugin skel

---
+++ bottom-admin-bar.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * Plugin Name: Bottom Admin Bar
+ * Plugin URI:  https://wordpress.org/plugins/TODO/
+ * Description: TODO
+ * Version:     0.0.1
+ * Author:      Themecraft Studio
+ * Author URI:  https://themecraft.studio/
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: bottom-admin-bar
+ * Domain Path: /languages
+ * GitHub Plugin URI: https://github.com/themecraftstudio/wordpress-bottom-admin-bar
+
+ Content Attachments is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 2 of the License, or
+ any later version.
+
+ Content Attachments is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Content Attachments. If not, see LICENSE.txt .
+ */
---
+++ readme.txt
@@ -0,0 +1,18 @@
+=== Content Attachments ===
+Contributors: themecraft
+Requires at least: 4.4.0
+Tested up to: 4.7.4
+Stable tag: trunk
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Adds the ability to move the admin bar to the bottom of the page from the Customizer
+
+== Description ==
+
+Main development repository at https://github.com/themecraftstudio/wordpress-bottom-admin-bar
+
+== Installation ==
+As with any other plugin hosted on wordpress.org
+
+== Changelog ==
