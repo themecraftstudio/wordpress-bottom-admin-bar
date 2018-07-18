9e95bc630b63fca4fb91953e6338ca4d031b3b0e
Ettore Del Negro <ettore@themecraft.studio>
2018-07-18 12:48:47 +0200

    Support keyboard shortcuts

--- admin-bar.js
+++ admin-bar.js
@@ -28,4 +28,17 @@ window.wpAdminBar = window.wpAdminBar || {
         document.querySelector('#wp-admin-bar-position > div')
             .addEventListener('click', window.wpAdminBar.togglePosition);
         });
+
+    document.addEventListener('keydown', (/* KeyboardEvent */ e) => {
+        if (e.key !== 'b' && e.key !== 'B')
+            return;
+
+        if ((e.ctrlKey || e.metaKey) && e.shiftKey) {
+            // ctrl/meta + alt + b: toggle position
+            window.wpAdminBar.togglePosition();
+        } else if (e.ctrlKey || e.metaKey) {
+            // ctrl/meta + b: toggle visibility
+            window.wpAdminBar.toggleVisibility();
+        }
+    })
 })();
